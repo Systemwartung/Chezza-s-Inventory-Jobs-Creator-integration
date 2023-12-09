663b9dd909189eb01a0674d92921eccf9993d1b9
Systemwartung <62596786+Systemwartung@users.noreply.github.com>
2023-12-09 14:09:06 +0100

    Add files via upload

---
+++ jobs_creator_intgration/client.lua
@@ -0,0 +1,48 @@
+local config = {}
+config.maxstashweight = 100000 -- weight of stashes.
+config.maxsafeweight = 100000 -- weight of safes.
+config.maxarmoryweight = 100000 -- weight of armories.
+
+RegisterNetEvent("jobs_creator:stash:openStash", function(markerId)
+    TriggerEvent('inventory:openInventory', {
+        id = "marker_" .. markerId,
+        type = "esx_job_creator_safe",
+        title = 'Stash - ' .. markerId,
+        weight = config.maxstashweight,
+        save = true,
+        timeout = 1000
+    })
+end)
+
+RegisterNetEvent("jobs_creator:safe:openSafe", function(markerId)
+    -- Example with Chezza's inventory
+    TriggerEvent('inventory:openInventory', {
+        id = "marker_" .. markerId,
+        type = "esx_job_creator_safe",
+        title = 'Safe - ' .. markerId,
+        weight = config.maxsafeweight,
+        save = true,
+        timeout = 1000
+    })
+end)
+
+RegisterNetEvent("jobs_creator:armory:openArmory", function(markerId)
+    -- Example with Chezza's inventory
+    TriggerEvent('inventory:openInventory', {
+        id = "marker_" .. markerId,
+        type = "esx_job_creator_armory",
+        title = 'Armory - ' .. markerId,
+        weight = config.maxarmoryweight,
+        save = true,
+        timeout = 1000
+    })
+end)
+
+RegisterNetEvent("jobs_creator:framework:ready", function() 
+    -- Disables the default script search (otherwise there would be 2 searches)
+    exports["jobs_creator"]:disableScriptEvent("jobs_creator:actions:search:searchPlayer")
+end)
+
+RegisterNetEvent("jobs_creator:actions:search:searchPlayer", function(targetServerId)
+    TriggerEvent('inventory:openPlayerInventory', targetServerId, true)
+end)
---
+++ jobs_creator_intgration/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Systemwartung'
+description 'Jobs-Creator Integration for Chezza Inventory'
+version '1.0'
+
+client_script 'client.lua'
